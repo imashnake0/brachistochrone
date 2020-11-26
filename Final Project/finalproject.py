fa22c3c59bd3e2da62fcaf992b2910d486bc3a16
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2020-11-26 16:50:17 -0500

    functioned
    - Created a brachistochrone function that takes in (x_0, y_0) and a and gives us a brachistochrone that (why?!) does not curve up.
    - Must make it (x_0, y_0, x, y), (where (x, y) is the final position) instead of (x_0, y_0, a)

--- Final Project/finalproject.py
+++ Final Project/finalproject.py
@@ -7,32 +7,35 @@ import math as mat
 #parameters
 a = 6
 
-def diffeqs(w, t, p):
-    [x, y, yd] = w # d matrix
-    [a] = p # parameters
-    der = [np.sqrt(y/((2*a)-y)), yd, 9.8]
-    return der
-
-# initial conditions
+# initial conditions (here, and final)
 x1 = 0
 y1 = 0
 #x2 = 
-#y2 =  
+#y2 = 
 
-# time scale
-t = np.linspace(0, 10, 1000)
+def brachistochrone(a, x1, y1):
+    def diffeqs(w, t, p):
+        [x, y, yd] = w # d matrix
+        [a] = p # parameters
+        der = [np.sqrt(y/((2*a)-y)), yd, 9.8]
+        return der
 
-# containing parameters and initial conditions
-p = [a]
-w0 = [x1, y1, 9.8]
+    # time scale
+    t = np.linspace(0, 10, 1000)
+
+    # containing parameters and initial conditions
+    p = [a]
+    w0 = [x1, y1, 9.8]
 
 
-wsol = odeint(diffeqs, w0, t, args = (p, ))
+    wsol = odeint(diffeqs, w0, t, args = (p, ))
 
-#plt.figure(figsize=(100, 100))
-#ax = plt.gca() #you first need to get the axis handle
-#ax.set_aspect() #sets the height to width ratio to 1.5. 
-plt.plot(wsol[:, 0], -1*wsol[:, 1], 'k-')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+    #plt.figure(figsize=(100, 100))
+    #ax = plt.gca() #you first need to get the axis handle
+    #ax.set_aspect() #sets the height to width ratio to 1.5. 
+    plt.plot(wsol[:, 0], -1*wsol[:, 1], 'k-')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+brachistochrone(a, x1, y1)
